4438138e3737ef16a771a052b2a68f69698a797a
github username <63208817+Thea-J@users.noreply.github.com>
2021-01-13 16:00:14 +0100

    basic list info

---
+++ list.py
@@ -0,0 +1,25 @@
+# Assign a list to variable x
+x = [1, 2, 3, 4]
+
+# Use square-bracket notation to retrieve list element
+# Note: list indices start at 0 
+y = x[0]
+
+# Use square-bracket notation to modify a list element
+x[2] = 7
+
+# Concatenate 2 lists 
+z = x + [y]
+
+# Delete the ith element in a list 
+# via del reserved word and square-bracket notation
+del x[3]
+
+# Return the length of a list 
+len(x)
+
+# Sort the elements in a list
+x.sort()
+
+# List of a list 
+a = [[1, 2], [3, 4]]
