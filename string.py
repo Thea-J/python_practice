6a17a1c5698fbde88f3e9e4643bbd83b971e183b
github username <63208817+Thea-J@users.noreply.github.com>
2021-01-13 16:18:44 +0100

    basic string info

---
+++ string.py
@@ -0,0 +1,13 @@
+# Define a string with double-quotes
+a = "Foo"
+
+# Define a string with single-quotes
+b = 'Bar'
+
+# Define a multi-line string with 3 double-quotes
+"""
+Multi-line
+"""
+
+# Define a nested quote (no-need to escape)
+"Hello 'Foo'" 
